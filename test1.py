a3943dae1992f6dbbdcdcc882ddefed04872f0c9
alkun <ak142@mail.ru>
2023-06-22 01:18:13 +0300

    slurm_hello2

--- test1.py
+++ test1.py
@@ -1,1 +1,10 @@
-print('Hello world')
+def append_to(element, to=[]):
+    to.append(element)
+    return to
+
+
+my_list = append_to(12)
+print(my_list)
+
+my_other_list = append_to(42)
+print(my_other_list)
