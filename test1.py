8546dddcf23033fd8e6efd3baf279dcdba7ec3d6
alkun <ak142@mail.ru>
2023-06-26 00:32:47 +0300

    func lambda no

--- test1.py
+++ test1.py
@@ -1,2 +1,10 @@
-func = lambda x, y: x + y
-print(func(1, 2))
+def append_to(element, to=[]):
+    to.append(element)
+    return to
+
+
+my_list = append_to(12)
+print(my_list)
+
+my_other_list = append_to(42)
+print(my_other_list)
