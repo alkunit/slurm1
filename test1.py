50ae6956263c56769480410bb4e2ae51907401c2
alkun <ak142@mail.ru>
2023-09-12 23:32:29 +0300

    test4

--- test1.py
+++ test1.py
@@ -1,25 +1,3 @@
-def ask_user(prompt, retries=3, hint="Ответьте, ДА или НЕТ?"):
-    while True:
-        retries -= 1
-        ok = input(prompt + " -> ").upper()
-
-        if ok in ("Д", "ДА"):
-            return True
-        elif ok in ("Н", "НЕТ"):
-            return False
-
-        if retries <= 0:
-            print("Не смог получить нужный ответ, считаю за отказ.")
-            return False
-        print(hint)
-
-# С ключевыми параметрами будут доступны также следующие варианты:
-# ask_user("Сохранить файл?", 0)
-# ask_user("Сохранить файл?", retries=1)
-# ask_user("Сохранить файл?", 2, "Жми Д или Н!!!")
-# и др.
-
-if ask_user("Сохранить файл?"):
-    print("Сохранил!")
-else:
-    print("Не сохранил.")
+True
+Приложение необходимо обновить.
+123q
